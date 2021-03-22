5bedf5d1bba5bb80d525f76490c0fdbb455c4196
jianghaibo <https://code.aliyun.com/eyoule/youleapi.git>
2021-03-22 17:37:31 +0800

    完善信息

--- Sns.Models/AutomapperProfiler.cs
+++ Sns.Models/AutomapperProfiler.cs
@@ -1,8 +1,4 @@
 ﻿using Sns.Domains.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Encodings.Web;
 using System.Web;
 
 namespace Sns.Models
@@ -11,9 +7,8 @@ namespace Sns.Models
     {
         public AutomapperProfiler()
         {
-            
             CreateMap<CommentCreateRequestDTO, Comment>().ForMember(dest=> dest.Body, mapper=> mapper.MapFrom(source=> HttpUtility.HtmlEncode(source.Body)));
-            CreateMap<FeedbackCreateRequestDTO, Feedback>();
+            CreateMap<FeedbackCreateRequestDTO, Feedback>().ForMember(dest => dest.Content, mapper => mapper.MapFrom(source => HttpUtility.HtmlEncode(source.Content)));
         }
     }
 }
